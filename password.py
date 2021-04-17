029accaf3f0e1c8f472d9011534c92e72a41dc4c
han201905 <63267166+han201905@users.noreply.github.com>
2021-04-16 23:58:05 -0700

    print password

--- password.py
+++ password.py
@@ -4,3 +4,7 @@ def password(n):
     chars = 'abcdefghijklmnopqrstuvwxyz1234567890'
     num = input("Password Length: ")
     num = int(num)
+    password = ''
+    for i in range(num):
+        password += random.choice(chars)
+    print(password)
